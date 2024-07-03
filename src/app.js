93d7ea665eaaceb880452c06105db2584a426f1e
victoriamartinez23 <victoriamartinezruiz23@gmail.com>
2024-07-03 17:51:48 +0000

    Carta terminada

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let números = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+let palos = ["♣", "♦", "♥", "♠"];
+
+function númeroRandom(array) {
+  let random = Math.floor(Math.random() * array.length);
+  return random;
+}
+
+const paloArriba = document.getElementById("palo-arriba");
+const número = document.getElementById("número");
+const paloAbajo = document.getElementById("palo-abajo");
+
+function cartaRandom() {
+  let númeroCarta = números[númeroRandom(números)];
+  let paloCarta = palos[númeroRandom(palos)];
+  paloArriba.innerHTML = paloCarta;
+  número.innerHTML = númeroCarta;
+  paloAbajo.innerHTML = paloCarta;
+
+  if (paloCarta === "♦" || paloCarta === "♥") {
+    paloArriba.style.color = "red";
+    paloAbajo.style.color = "red";
+  }
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log(cartaRandom());
 };
