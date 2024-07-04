7eb1e1d8d7350831651b7993a89c12c72c7e673e
victoriamartinez23 <victoriamartinezruiz23@gmail.com>
2024-07-04 13:17:16 +0000

    Eliminación de código innecesario y de tildes

--- src/app.js
+++ src/app.js
@@ -2,26 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-let números = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+let numeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 let palos = ["♣", "♦", "♥", "♠"];
 
-function númeroRandom(array) {
+function numeroRandom(array) {
   let random = Math.floor(Math.random() * array.length);
   return random;
 }
 
 const paloArriba = document.getElementById("palo-arriba");
-const número = document.getElementById("número");
+const numero = document.getElementById("numero");
 const paloAbajo = document.getElementById("palo-abajo");
 
 function cartaRandom() {
-  let númeroCarta = números[númeroRandom(números)];
-  let paloCarta = palos[númeroRandom(palos)];
+  let numeroCarta = numeros[numeroRandom(numeros)];
+  let paloCarta = palos[numeroRandom(palos)];
   paloArriba.innerHTML = paloCarta;
-  número.innerHTML = númeroCarta;
+  numero.innerHTML = numeroCarta;
   paloAbajo.innerHTML = paloCarta;
 
   if (paloCarta === "♦" || paloCarta === "♥") {
